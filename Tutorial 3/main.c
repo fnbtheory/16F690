01e8ef0f5ec2d3438845c02fb008d649ed178308
Kyluke McDougall <kyluke@kyco.co.za>
2013-08-24 16:53:09 +0200

    Removed broken files
    Replaced missing files
    
    Removed random text before and after code

--- Tutorial 3/main.c
+++ Tutorial 3/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <16F690.h>
 #device adc=8
 
@@ -19,61 +18,61 @@
 
 void main(void)
 {
-	SET_TRIS_C(192);
-	button();
+	SET_TRIS_C(192); // Sets the last 2 pins of port C to inputs 1100 000
+	button(); // Calls the function button()
 }
 
 void button()
 {
-    while(true)
+    while(true) // Loop forever
 	{
-    	output_c(0);
+    	output_c(0); // Switch off the LEDs for blinking effect
     	
-        if(input(C7) && input(C6))
+        if(input(C7) && input(C6)) // If both buttons are pressed
         {
-            reset();
+            reset(); // Reset the count
         }
-        else if(input(C6))
+        else if(input(C6)) // If port C6 is activated
         {
-            down_count();
+            down_count(); // Count down once and display
         }
-        else if(input(C7))
+        else if(input(C7)) // If port C7 is activated
         {
-            up_count();
+            up_count(); // Count up once and display
         }
-        display();
+        display(); // Display everytime and when nothing is pressed.
     }   
 }
 
 void reset()
 {
-    count = 0;
+    count = 0; // Sets the global variable to 0, which means count == 0
 }
 
 void up_count()
 {
-    if (count < 15)
+    if (count < 15) // Prevent count from going higher than 15
     {
-        count++;
-        delay_ms(100);
-        display(); 
+        count++; // Increment the global variable
+        delay_ms(100); // Delay for 100ms + 100ms in display() to create a 200ms delay
+        display(); // Display the new value
     }
 }       
 
 void down_count()
 {
-    if (count > 0)
+    if (count > 0) // Prevent the count every going below 0
     {
-        count--;
-        delay_ms(100);
-        display();   
+        count--; // Decrement the global variable
+        delay_ms(100); // Delay for 100ms + 100ms in display() to create a 200ms delay
+        display(); // Display the new value
     }
 }
 
 void display()
 {
-    delay_ms(100);
-    output_c(count);
+    delay_ms(100); // Wait 100ms before displaying anything
+    output_c(count); // Display the value of count on the LEDs
 }    
 
 //              /)/)
@@ -82,95 +81,3 @@ void display()
 //            /#/
 //           /#/	Tighty Whities (Pty) Ltd.
 //          /#/		(c) 2013. For Ze Party.
-=======
-#include <16F690.h>
-#device adc=8
-
-#FUSES NOWDT                    //No Watch Dog Timer
-#FUSES HS
-#FUSES INTRC_IO                 //Low power osc < 200 khz  LP
-#FUSES NOPROTECT                //Code not protected from reading
-#FUSES NOBROWNOUT               //No brownout reset
-#FUSES NOMCLR                   //Master Clear pin used for I/O
-#FUSES NOCPD                    //No EE protection
-#FUSES NOPUT                    //No Power Up Timer
-#FUSES NOIESO                   //Internal External Switch Over mode disabled
-#FUSES NOFCMEN                  //Fail-safe clock monitor disabled
-
-#include "main.h"
-
-#use delay(clock=32000)
-
-void main(void)
-{
-	SET_TRIS_C(192);
-	while(true)
-	{
-    	switch(button())
-    	{
-        	case 1: reset(); break;
-        	case 2: down_count(); break;
-        	case 3: up_count(); break;
-        	default:
-        	    delay_ms(100);
-                output_c(count);
-                break;
-        }           
-	}
-}
-
-int button()
-{
-    if(!input(C7) && !input(C6))
-    {
-        return 1;
-    }
-    else if(!input(C6))
-    {
-        return 2;
-    }
-    else if(!input(C7))
-    {
-        return 3;
-    }
-    else
-        return 0;
-}
-
-void reset()
-{
-    output_c(16);
-}
-
-void up_count()
-{
-    count++;
-    if (count <= 15)
-    {
-        delay_ms(200);
-        display();
-    }
-}       
-
-void down_count()
-{
-    count--;
-    if (count <= 15)
-    {
-        delay_ms(200);
-        display();
-    }
-}
-
-void display()
-{
-    output_c(count);
-}    
-
-//              /)/)
-//             ( 0 0)
-//             /'-._)
-//            /#/
-//           /#/	Tighty Whities (Pty) Ltd.
-//          /#/		(c) 2013. For Ze Party.
->>>>>>> 6fd66c6cc267f8b1d861c4e19ebd960bad0c6b64
--- Tutorial 3/main.h
+++ Tutorial 3/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define C0 PIN_C0
 #define C1 PIN_C1
 #define C2 PIN_C2
@@ -13,19 +12,3 @@ void up_count();
 void down_count();
 void display();
 void reset();
-=======
-#define C0 PIN_C0
-#define C1 PIN_C1
-#define C2 PIN_C2
-#define C3 PIN_C3
-#define C6 PIN_C6
-#define C7 PIN_C7
-
-int count = 0;
-
-int button();
-void up_count();
-void down_count();
-void display();
-void reset();
->>>>>>> 6fd66c6cc267f8b1d861c4e19ebd960bad0c6b64
